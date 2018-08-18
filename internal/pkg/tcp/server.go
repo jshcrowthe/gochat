7191e1730aab59e8dcca6a776fd6abb3f4e46b1f
Josh Crowther <jshcrowthe@gmail.com>
2018-08-18 07:56:10 -0700

    WIP: Refactoring some more

--- internal/pkg/tcp/server.go
+++ internal/pkg/tcp/server.go
@@ -7,6 +7,20 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+func handleConnections(server net.Listener) {
+	// Infinite loop that accepts all new clients
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Debugf("Client connected from: %v", conn.RemoteAddr())
+
+		// Handle future interactions with this client
+		go handleClient(conn)
+	}
+}
+
 // Start - Creates the actual chat server
 func Start(ip string, port int, logfile string) {
 	// Start the TCP server
